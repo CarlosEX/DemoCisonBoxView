84a00e656a2bd3f25a444e732b46bc8db7c5132a
Carlos Antonio <carlos22martinssilva@gmail.com>
2021-08-16 13:29:23 -0300

    Adicionar arquivos de projeto.

---
+++ DemoCisonBoxView/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace DemoCisonBoxView {
+    partial class Form1 {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.buttonLight = new System.Windows.Forms.Button();
+            this.buttonDark = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonLight
+            // 
+            this.buttonLight.Location = new System.Drawing.Point(26, 35);
+            this.buttonLight.Name = "buttonLight";
+            this.buttonLight.Size = new System.Drawing.Size(112, 23);
+            this.buttonLight.TabIndex = 0;
+            this.buttonLight.Text = "Light";
+            this.buttonLight.UseVisualStyleBackColor = true;
+            // 
+            // buttonDark
+            // 
+            this.buttonDark.Location = new System.Drawing.Point(156, 35);
+            this.buttonDark.Name = "buttonDark";
+            this.buttonDark.Size = new System.Drawing.Size(112, 23);
+            this.buttonDark.TabIndex = 1;
+            this.buttonDark.Text = "Dark";
+            this.buttonDark.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 90);
+            this.Controls.Add(this.buttonDark);
+            this.Controls.Add(this.buttonLight);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLight;
+        private System.Windows.Forms.Button buttonDark;
+    }
+}
+
---
+++ DemoCisonBoxView/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using Cison.BoxView;
+
+namespace DemoCisonBoxView {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            LoadEvents();
+        }
+        private void LoadEvents() {
+            this.buttonDark.Click += ButtonDark_Click;
+            this.buttonLight.Click += ButtonLigh_Click;
+        }
+        private void ButtonLigh_Click(object sender, EventArgs e) {
+            BoxViewFactory.Show(IconBox.Erro, "Erro ao cadastrar", ThemeBox.Light, BoxViewBorderStyle.Single);
+            BoxViewFactory.Show(IconBox.Success, "Cadastro feito com sucesso", ThemeBox.Light, BoxViewBorderStyle.None);
+        }
+        private void ButtonDark_Click(object sender, EventArgs e) {
+            BoxViewFactory.Show(IconBox.Erro, ThemeBox.Dark, BoxViewBorderStyle.None);
+            BoxViewFactory.Show(IconBox.Success, ThemeBox.Dark, BoxViewBorderStyle.Single);
+        }
+    }
+}
